3e332e26a52d582ab9516cf8fedc4328e1105c36
Corentin <corentin.maurer@ecal.ch>
2025-03-28 09:48:03 +0100

    add sliders

--- maurer-roses/src/App.js
+++ maurer-roses/src/App.js
@@ -1,20 +1,71 @@
 import BaseApp from "./BaseApp";
+import { createSlidersContainer, createSlider } from "./Sliders.js"; // Import des fonctions
 
 export default class App extends BaseApp {
   constructor() {
     super();
     this.init();
+    this.setCanvasResolution();
     this.ctx = this.canvas.getContext("2d");
     this.width = this.canvas.width = window.innerWidth;
     this.height = this.canvas.height = window.innerHeight;
     this.n = 2;
     this.d = 29;
-    this.progress = 0; // Progression du dessin
-    this.animate();
+    this.progress = 360; // Initialisation de la progression à 360 degrés
+
+    this.slidersContainer = createSlidersContainer(); // Créer le container des sliders
+    this.createSliders(); // Créer les sliders
+    this.draw();
   }
 
   init() {
     this.canvas.style.backgroundColor = "black";
+  }
+
+  setCanvasResolution() {
+    const dpr = window.devicePixelRatio || 1;
+    this.canvas.width = this.width * dpr;
+    this.canvas.height = this.height * dpr;
+    this.ctx.scale(dpr, dpr);
+  }
+
+  createSliders() {
+    this.nSlider = createSlider(
+      "n",
+      1,
+      10,
+      this.n,
+      1,
+      this.slidersContainer,
+      (label, value) => {
+        this[label] = value;
+        this.draw();
+      }
+    );
+    this.dSlider = createSlider(
+      "d",
+      1,
+      50,
+      this.d,
+      1,
+      this.slidersContainer,
+      (label, value) => {
+        this[label] = value;
+        this.draw();
+      }
+    );
+    this.progressSlider = createSlider(
+      "Progression",
+      0,
+      360,
+      this.progress,
+      1,
+      this.slidersContainer,
+      (label, value) => {
+        this[label] = value;
+        this.draw();
+      }
+    );
   }
 
   draw() {
@@ -26,14 +77,14 @@ export default class App extends BaseApp {
     ctx.save();
     ctx.translate(this.width / 2, this.height / 2);
 
-    let alpha = this.progress / 361; // Opacité de 0 à 1
-    ctx.strokeStyle = `rgba(255, 255, 255, ${alpha})`;
+    ctx.strokeStyle = `rgba(255, 255, 255, 1)`;
     ctx.lineWidth = 2;
 
     ctx.beginPath();
 
-    for (let i = 0; i < this.progress; i++) {
-      // Dessiner progressivement
+    const maxProgress = Math.min(360, this.progress); // Limiter la progression à 360 degrés
+    for (let i = 0; i <= maxProgress; i += 2) {
+      // Dessiner jusqu'à la valeur de progression
       let k = i * (Math.PI / 180) * this.d;
       let r = 500 * Math.sin(this.n * k);
       let x = r * Math.cos(k);
@@ -48,16 +99,5 @@ export default class App extends BaseApp {
 
     ctx.stroke();
     ctx.restore();
-  }
-
-  animate() {
-    if (this.progress < 361) {
-      this.progress += 2; // Augmenter la progression
-    } else {
-      this.progress - 2; // Réinitialiser
-    }
-
-    this.draw();
-    requestAnimationFrame(this.animate.bind(this)); // Boucle d'animation
   }
 }
